1a116bffbb8cb469754b8af552b517a370c6ce52
Zalezny <zaleznypl@gmail.com>
2023-01-13 08:57:47 +0100

    fixed, without const database

---
+++ app/src/main/java/com/example/outfitapp/fragments/BottomSheetFragment.kt
@@ -0,0 +1,124 @@
+
+
+package com.example.outfitapp.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import com.example.outfitapp.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.gson.JsonObject
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.bottomsheet_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //here code
+        val etOutfitName: EditText = view.findViewById(R.id.et_outfit_name)
+        // onClick "enter" or "done" button
+        etOutfitName.setOnEditorActionListener { _, i, _ ->
+            if (i == EditorInfo.IME_ACTION_DONE) {
+
+                val outfitName = etOutfitName.text.toString()
+
+                postToServer(outfitName)
+
+                //clear text in edit text
+                etOutfitName.text.clear()
+            }
+            false
+        }
+
+
+    }
+
+    private fun postToServer(amount: String) {
+
+        val json = JsonObject()
+        json.addProperty("title", amount)
+
+        //create time
+        val currentTime = SimpleDateFormat("HH:mm:ss", Locale.US).format(Date())
+
+        json.addProperty("hour", currentTime)
+
+        val okHttpClient = OkHttpClient()
+        val request = Request.Builder()
+            .post(json.toString().toRequestBody("application/json".toMediaType()))
+            .url(ConstDatabase.ADD_OUTFIT_URL)
+            .addHeader("authorization", ConstDatabase.OUTFIT_KEY)
+            .build()
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+
+                Log.d("FailureResponse", "$e")
+                textMsg("Failed! Contact to Danielczyk!")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+                if (response.isSuccessful)
+                {
+                    //request message
+                    val bodyMsg = response.body!!.string()
+
+                    val jsonTok = JSONTokener(bodyMsg).nextValue() as JSONObject
+                    val id = jsonTok.getString("id")
+
+
+
+                    activity!!.runOnUiThread {
+                        //set to bundle outfitName and mainID
+                        val bundle = bundleOf(
+                            "outfitName" to amount,
+                            "mainID" to id
+                        )
+                        //set Fragment Result (invoke in MainActivity)
+                        setFragmentResult("BottomSheetFragmentRequest", bundle)
+
+                        //remove BottomSheetFragment
+                        dismiss()
+                    }
+                }
+
+
+            }
+        })
+    }
+
+    fun textMsg(msg: String) {
+        Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show()
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/example/outfitapp/stopwatch/StopwatchService.kt
@@ -0,0 +1,198 @@
+package com.example.outfitapp.stopwatch
+
+import android.app.Notification.FOREGROUND_SERVICE_IMMEDIATE
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import com.example.outfitapp.*
+import com.example.outfitapp.util.StopwatchUtil
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class StopwatchService :  LifecycleService() {
+
+    companion object {
+        //is on or off (LiveData)
+        val stopwatchEvent = MutableLiveData<StopwatchEvent>()
+        //time in milliseconds (LiveData)
+        val timerInMillis = MutableLiveData<Long>()
+        //date in json format (just String) (LiveData)
+        val dateJSONFormat = MutableLiveData<String>()
+    }
+
+    //check is service stopped
+    private var isServiceStopped = false
+
+    //Timer properties
+    private var lapTime = 0L
+    private var timeStarted = 0L
+
+    //date of start timer
+    private var dateStart = ""
+
+    //Variables between intents exchange
+    private var outfitName = ""
+    private var chosenID = ""
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationBuilder: NotificationCompat.Builder
+
+
+
+    override fun onCreate() {
+        super.onCreate()
+        //reset stopwatch on Create.
+        initValues()
+
+
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        //extras will be use when user click on notification
+        chosenID = intent!!.getStringExtra("chosenID")!!
+        outfitName = intent.getStringExtra("titleOutfit")!!
+        //action service (set in StoperFragment)
+        intent.let {
+            when(it.action) {
+                ACTION_START_SERVICE -> {
+                    startForegroundService()
+                }
+                ACTION_STOP_SERVICE -> {
+                    stopService()
+                }
+                else -> {
+                    return@let
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForegroundService() {
+        //send info about start Stopwatch
+        stopwatchEvent.postValue(StopwatchEvent.START)
+        startTimer()
+
+        //sdk must be over of oreo!
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        }
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+
+        //observer of timerInMillis to change timer in notification text
+        timerInMillis.observe(this) {
+            if(!isServiceStopped) {
+                notificationBuilder.setContentText(
+                    StopwatchUtil.getFormattedTime(it, false)
+                )
+                //notify manager about changes
+                notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build())
+            }
+        }
+
+    }
+
+    private fun stopService() {
+        isServiceStopped = true
+        //send date of start
+        dateJSONFormat.postValue(dateStart)
+        //set values to default value
+        initValues()
+        //cancel foreground notification
+        (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager).cancel(
+            NOTIFICATION_ID
+        )
+        stopForeground(true)
+        //stop this service (itself)
+        stopSelf()
+    }
+
+    private fun initValues() {
+        //finish stopwatch
+        stopwatchEvent.postValue(StopwatchEvent.END)
+        //set time on 0
+        timerInMillis.postValue(0L)
+    }
+
+    private fun createNotificationChannel() {
+        //channel for notification
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_LOW
+        )
+
+        //init notification Manager
+        notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        //add channel to notification Manager
+        notificationManager.createNotificationChannel(channel)
+
+
+        //init notification Builder
+        notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.stoper_go)
+            .setContentTitle(outfitName)
+            .setContentText("00:00:00")
+            .setForegroundServiceBehavior(FOREGROUND_SERVICE_IMMEDIATE)
+            .setContentIntent(pendingIntentTimeActivity())
+
+    }
+
+    private fun pendingIntentTimeActivity(): PendingIntent {
+
+        val intent = Intent(this, TimeActivity::class.java).apply {
+            //if activity is on the top of the history stack, it wont be launch
+            this.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+        //intents needs in TimeActivity/StoperFragment to start
+        intent.putExtra("title", outfitName)
+        intent.putExtra("chosenOutfitID", chosenID)
+
+        //intent for notification after click on it
+
+        return PendingIntent.getActivity(
+            this,
+            420,
+            intent,
+            PendingIntent.FLAG_MUTABLE
+        )
+    }
+
+
+    private fun startTimer() {
+        //set start time the current time in millis
+        timeStarted = System.currentTimeMillis()
+        //set date of start timer with format json
+        dateStart = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+        //open new task for timer
+        CoroutineScope(Dispatchers.Main).launch {
+        //while stopwatchEvent.value will be START and service will be not stopped
+            while (stopwatchEvent.value!! == StopwatchEvent.START && !isServiceStopped) {
+                //the timer time is current time minus time of started
+                lapTime = System.currentTimeMillis() - timeStarted
+                //send info about timer time (for ui)
+                timerInMillis.postValue(lapTime)
+                //delay of 50 milliseconds
+                delay(50L)
+            }
+        }
+    }
+
+
+}
